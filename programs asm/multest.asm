4e4557813784a230693b7312cf01c5c43df3cfc4
warang <warangkana_chit@cmu.ac.th>
2025-10-19 18:07:24 +0700

    edit asm

--- programs asm/multest.asm
+++ programs asm/multest.asm
@@ -1,16 +1,16 @@
         ; assume data section at bottom:
         ; labels: A, B, ONE, NEGONE, ZERO
 
-        lw R1, A(R0)       ; R1 = a (multiplicand)
-        lw R2, B(R0)       ; R2 = b (multiplier)
-        lw R3, ZERO(R0)    ; R3 = 0   (product)
-        lw R5, ONE(R0)     ; R5 = 1
-        lw R4, NEGONE(R0)  ; R4 = -1  (for decrement)
-Loop:   beq R2, R0, Done   ; if b == 0 goto Done
-        add R3, R3, R1     ; product = product + a
-        add R2, R2, R4     ; b = b + (-1)  -> decrement
-        beq R0, R0, Loop   ; unconditional jump: if R0==R0 true -> branch to Loop (offset negative to go back)
-Done:   sw R3, RESULT(R0)  ; store product to RESULT
+        lw R1, A(R0)                    ; R1 = a (multiplicand)
+        lw R2, B(R0)                    ; R2 = b (multiplier)
+        lw R3, ZERO(R0)                 ; R3 = 0   (product)
+        lw R5, ONE(R0)                  ; R5 = 1
+        lw R4, NEGONE(R0)               ; R4 = -1  (for decrement)
+Loop:   beq R2, R0, Done                ; if b == 0 goto Done
+        add R3, R3, R1                  ; product = product + a
+        add R2, R2, R4                  ; b = b + (-1)  -> decrement
+        beq R0, R0, Loop                ; unconditional jump: if R0==R0 true -> branch to Loop (offset negative to go back)
+Done:   sw R3, RESULT(R0)               ; store product to RESULT
         halt
 
 ; --- data (word addresses) ---
