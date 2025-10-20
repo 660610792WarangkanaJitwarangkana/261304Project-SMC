abcdf3410ea1862c85df8355a22ed368935404b1
warang <warangkana_chit@cmu.ac.th>
2025-10-20 18:22:56 +0700

    edit comb asm

--- programs/combination.asm
+++ programs/combination.asm
@@ -1,57 +1,31 @@
-        lw      0       4       stack       ; initialize SP
-        lw      0       5       combA       ; load function address
-        lw      0       6       nVal        ; n = 5
-        lw      0       7       rVal        ; r = 3
-        jalr    5       5                   ; call comb(n, r)
-        halt
+        lw      0       1       nval        ; reg1 = n
+        lw      0       2       rval        ; reg2 = r
+        lw      0       3       one         ; reg3 = 1
+        lw      0       4       zero        ; reg4 = 0
+        lw      0       5       neg1        ; reg5 = -1
+        lw      0       6       result      ; reg6 = 1 (result)
+        lw      0       7       tmp         ; reg7 = temp counter
 
-; function comb(n, r)
-comb    add     4       4       -1          ; push space for RA
-        sw      4       5       0
-        add     4       4       -1          ; push n
-        sw      4       6       0
-        add     4       4       -1          ; push r
-        sw      4       7       0
+        beq     2       0       base        ; if r == 0 return 1
+        beq     1       2       base        ; if n == r return 1
 
-        ; if (r == 0) return 1
-        beq     7       0       baseC
+        lw      0       7       zero        ; i = 0
 
-        ; if (n == r) return 1
-        add     6       -7      1           ; temp = n - r
-        beq     1       0       baseC
+loop    beq     7       2       done        ; if i == r stop
+        add     1       5       1           ; n = n - 1
+        add     6       1       6           ; result += n
+        add     7       3       7           ; i++
+        beq     0       0       loop        ; repeat
 
-        ; recursive call 1: comb(n-1, r)
-        add     6       -1      6           ; n = n - 1
-        jalr    5       5
-        lw      4       1       0           ; save result1
+done    halt
 
-        ; recursive call 2: comb(n-1, r-1)
-        add     6       -1      6
-        add     7       -1      7
-        jalr    5       5
-        lw      4       2       0           ; save result2
+base    halt
 
-        ; add result1 + result2
-        add     1       2       3
-        beq     0       0       combR
-
-baseC
-        lw      0       3       one         ; result = 1
-
-combR
-        ; pop stack
-        lw      4       7       0
-        add     4       4       1
-        lw      4       6       0
-        add     4       4       1
-        lw      4       5       0
-        add     4       4       1
-        jalr    5       0                   ; return
-
-; Data section
-stack     .fill   1024
-combA     .fill   comb
-nVal      .fill   5
-rVal      .fill   3
-one       .fill   1
-neg1      .fill   -1
+; ===== Data =====
+nval    .fill   5
+rval    .fill   3
+one     .fill   1
+zero    .fill   0
+neg1    .fill   -1
+result  .fill   1
+tmp     .fill   0
