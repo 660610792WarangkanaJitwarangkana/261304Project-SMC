6f893341749f04ab039bffa3dcca32cb1df070d0
warang <warangkana_chit@cmu.ac.th>
2025-10-20 18:18:48 +0700

    edit comb

--- programs/combination.asm
+++ programs/combination.asm
@@ -1,5 +1,5 @@
-        lw      0       4       stackBase   ; initialize SP
-        lw      0       5       combAddr    ; load function address
+        lw      0       4       stack       ; initialize SP
+        lw      0       5       combA       ; load function address
         lw      0       6       nVal        ; n = 5
         lw      0       7       rVal        ; r = 3
         jalr    5       5                   ; call comb(n, r)
@@ -14,11 +14,11 @@ comb    add     4       4       -1          ; push space for RA
         sw      4       7       0
 
         ; if (r == 0) return 1
-        beq     7       0       baseCase
+        beq     7       0       baseC
 
         ; if (n == r) return 1
         add     6       -7      1           ; temp = n - r
-        beq     1       0       baseCase
+        beq     1       0       baseC
 
         ; recursive call 1: comb(n-1, r)
         add     6       -1      6           ; n = n - 1
@@ -33,12 +33,12 @@ comb    add     4       4       -1          ; push space for RA
 
         ; add result1 + result2
         add     1       2       3
-        beq     0       0       returnComb
+        beq     0       0       combR
 
-baseCase
+baseC
         lw      0       3       one         ; result = 1
 
-returnComb
+combR
         ; pop stack
         lw      4       7       0
         add     4       4       1
@@ -49,8 +49,8 @@ returnComb
         jalr    5       0                   ; return
 
 ; Data section
-stackBase .fill   1024
-combAddr  .fill   comb
+stack     .fill   1024
+combA     .fill   comb
 nVal      .fill   5
 rVal      .fill   3
 one       .fill   1
